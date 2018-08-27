a21d924b52156a8778a039492fd9a2f2d46767b1
Aleksandr <aleksandr230481@gmail.com>
2018-08-27 14:47:36 +0500

    27.08.2018

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
-var syntax        = 'sass'; // Syntax: sass or scss;
+//var syntax        = 'sass'; // Syntax: sass or scss;
 
-var gulp          = require('gulp'),
+var gulp          = require('gulp'), // Подключаем Gulp
 		gutil         = require('gulp-util' ),
-		sass          = require('gulp-sass'),
-		browserSync   = require('browser-sync'),
-		concat        = require('gulp-concat'),
-		uglify        = require('gulp-uglify'),
-		//cleancss      = require('gulp-clean-css'),
-		rename        = require('gulp-rename'),
-		autoprefixer  = require('gulp-autoprefixer'),
+		sass          = require('gulp-sass'), //Подключаем Sass пакет,
+		browserSync   = require('browser-sync'), // Подключаем Browser Sync
+		concat        = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+		uglify        = require('gulp-uglify'), // Подключаем gulp-uglifyjs (для сжатия JS)
+		cleancss      = require('gulp-clean-css'), // Подключаем пакет для минификации CSS
+		rename        = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+    	del           = require('del'); // Подключаем библиотеку для удаления файлов и папок
+		autoprefixer  = require('gulp-autoprefixer'), // Подключаем библиотеку для автоматического добавления префиксов
 		notify        = require("gulp-notify"),
 		rsync         = require('gulp-rsync');
+
 
 gulp.task('browser-sync', function() {
 	browserSync({
@@ -24,25 +26,25 @@ gulp.task('browser-sync', function() {
 	})
 });
 
-gulp.task('styles', function() {
-	return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
-	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+gulp.task('sass', function() { // Создаем таск Sass
+	return gulp.src('app/sass/**/*.sass') // Берем источник
+	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError())) // Преобразуем Sass в CSS посредством gulp-sass
 	.pipe(rename({ suffix: '.min', prefix : '' }))
-	.pipe(autoprefixer(['last 15 versions']))
-	//.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
-	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.stream())
+	.pipe(autoprefixer(['last 15 versions']))// Создаем префиксы
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+	.pipe(browserSync.stream()) // Обновляем CSS на странице при изменении
 });
 
 gulp.task('js', function() {
 	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',
+        'app/libs/jquery/dist/jquery.min.js',
         'app/libs/bootstrap.min.js',
         'app/libs/isotope-layout/isotope.pkgd.min.js',
         'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
-	// .pipe(uglify()) // Mifify js (opt.)
+	.pipe(uglify()) // Mifify js (opt.)
 	.pipe(gulp.dest('app/js'))
 	.pipe(browserSync.reload({ stream: true }))
 });
@@ -62,10 +64,37 @@ gulp.task('rsync', function() {
 	}))
 });
 
-gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
-	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
+gulp.task('clean', function() {
+    return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+
+gulp.task('build', ['removedist', 'sass', 'js'], function() {
+
+    var buildCss = gulp.src([ // Переносим библиотеки в продакшен
+        'app/css/main.min.css',
+    ]).pipe(gulp.dest('dist/css'));
+
+    var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
+        .pipe(gulp.dest('dist/fonts'));
+
+    var buildJs = gulp.src('app/js/**/*scripts.min.js') // Переносим скрипты в продакшен
+        .pipe(gulp.dest('dist/js'));
+
+    var buildImg = gulp.src('app/img/**/*') // Переносим скрипты в продакшен
+        .pipe(gulp.dest('dist/img'));
+
+    var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
+        .pipe(gulp.dest('dist'));
+
+});
+gulp.task('removedist', function() { return del.sync('dist'); });
+gulp.task('clear', function () {return cache.clearAll();})
+
+gulp.task('watch', ['sass', 'js', 'browser-sync'], function() {
+	gulp.watch('app/sass/**/*.sass', ['sass']);
 	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
 	gulp.watch('app/*.html', browserSync.reload)
 });
+
 
 gulp.task('default', ['watch']);
